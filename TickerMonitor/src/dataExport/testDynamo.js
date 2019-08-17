32369bb8fa7c6959c01f92e8f3d1ab56011d7c04
Romain Lussier <romdj_123@hotmail.com>
2019-08-17 05:18:35 +0200

    cleaning DynamoDB module

--- TickerMonitor/src/dataExport/testDynamo.js
+++ TickerMonitor/src/dataExport/testDynamo.js
@@ -1,29 +1,14 @@
 'use strict';
 
-const hash = require('object-hash');
-const yahooFinance = require('yahoo-finance');
-const { writeFileSync, appendFileSync, readFileSync, copyFileSync } = require('fs');
+const { appendFileSync, readFileSync } = require('fs');
 const AWS = require('aws-sdk');
-const uuid = require('uuid/v4');
-const request = require('request-promise');
-
-const dynamodbConfig = require(`${process.cwd()}/config/dynamo.json`);
 
 AWS.config.apiVersions = { dynamodb: '2012-08-10' };
 AWS.config.update({ region: 'us-east-1' });
 
-let counter = 0;
 const dynamodb = new AWS.DynamoDB();
-const dateList = [];
-const dateRange = { begin: '2018-01-01', end: '2018-12-31' };
-const tickers = [...NYSE, ...NASDAQ];
-const results = [];
-const errTickers = [];
 const maxItemPerQuery = 25;
 const errDataPath = `${process.cwd()}/ResultData/ErrorTickers_day-to-day.json`;
-const errLogPath = `${process.cwd()}/ResultData/error_day-to-day.log`;
-const resultDataPath = `${process.cwd()}/ResultData/resultorTickers_day-to-day.json`;
-const resultLogPath = `${process.cwd()}/ResultData/result_day-to-day.log`;
 const dynamoFailedPath = `${process.cwd()}/ResultData/dynamo_failedQueries.json`;
 
 const formatDataDynamo = (quotes) => {
@@ -32,22 +17,18 @@ const formatDataDynamo = (quotes) => {
     if (quotes.length > maxItemPerQuery) {
       for (let index = 0; index < quotes.length; index += maxItemPerQuery) {
         const endIndex = (index + 24 > quotes.length) ? quotes.length - 1 : index + 24;
-        // console.log(`splicing quotes, length: ${quotes.length}`);
         sizedQuotes.push(quotes.slice(index, endIndex));
       }
-      // while (quotes.length > 0)
-      //   console.log(`splicing quotes, length: ${quotes.length}`);
-      // sizedQuotes.push(quotes.splice(0, quotes.length > maxItemPerQuery ? 24 : quotes.length));
       return sizedQuotes;
     }
   }
   return [quotes];
 };
 
-const dispatchDynamo = (quotes) => {
+const dispatchDynamo = (quotes, tableName) => {
   if (JSON.stringify(quotes).length > 2 || quotes.length !== 0) {
     if (JSON.stringify(quotes).length < 15000 && quotes.length <= maxItemPerQuery) {
-      const query = generateQuery(quotes);
+      const query = generateQuery(quotes, tableName);
       appendFileSync(errDataPath, JSON.stringify(query), 'utf8');
       dynamodb.batchWriteItem(query, function (err, data) {
         if (err) {
@@ -101,7 +82,7 @@ const generateQuery = (objects) => {
       }
     }
   ));
-  return { RequestItems: { "trading": items } };
+  return { RequestItems: { tableName: items } };
 };
 
 const dispatchFailedQueries = () => {
@@ -118,9 +99,9 @@ const dispatchFailedQueries = () => {
   });
 }
 
-function insertMany(dataArray, dbName, tableName) {
+function insertMany(dataArray, tableName) {
   formatDataDynamo(dataArray)
-    .forEach(formatQuotes => dispatchDynamo(formatQuotes));
+    .forEach(formatQuotes => dispatchDynamo(formatQuotes, tableName));
 }
 module.exports = {
   insertOne, insertMany, deleteOne, deleteMany
